5ce0769270618b2256bdc40ed931112f381cfaf9
KJM <kjm99d@naver.com>
2024-04-07 22:48:28 +0900

    🚑 관리자/사용자 토큰 발급 우선순위 변경

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -9,8 +9,6 @@ const UserService = require('./../services/userService');
 const PolicyService = require('./../services/policyService');
 const ErrorCodes = require('../errorCodes');
 
-const { AccessAdmin } = require('./../middlewares/TokenValidate')
-
 // 로그인
 router.post('/login', async (req, res) => {
     var code = ErrorCodes.SUCCESS;
@@ -18,14 +16,6 @@ router.post('/login', async (req, res) => {
     const connection = await mysql.createConnection(dbConfig);
 
     try {
-        // 제품 유효성 검사
-        const productInfo = await ProductService.FindProduct(connection, product);
-        if (ErrorCodes.SUCCESS != productInfo.code) {
-            return res.json(productInfo);
-        }
-
-        const productId = productInfo.data[0].id;
-
         // 사용자 ID 조회
         const userId = await UserService.FindUserId(connection, username, passwd);
         if (ErrorCodes.SUCCESS != userId.code) {
@@ -34,6 +24,22 @@ router.post('/login', async (req, res) => {
 
         const m_userId = userId.data[0].id;
         const m_userRole = userId.data[0].isAdmin;
+
+        // 관리자 토큰 발급
+        if (1 == m_userRole)
+        {
+            const token = jwt.sign({ "user" : m_userId, "isAdmin" : m_userRole }, SecretKey_JWT, { expiresIn: '24h' });
+            return res.json({ code, "data" : [token] });
+        }
+
+
+        // 제품 유효성 검사
+        const productInfo = await ProductService.FindProduct(connection, product);
+        if (ErrorCodes.SUCCESS != productInfo.code) {
+            return res.json(productInfo);
+        }
+
+        const productId = productInfo.data[0].id;
 
         // 사용자의 제품 만료기간 조회
         const userProduct = await UserService.FindUserProduct(connection, m_userId, productId);
@@ -66,9 +72,9 @@ router.post('/login', async (req, res) => {
             return res.json({ code });
         }
 
-        // Access Token 발급
+        // 일반 사용자 토큰 발급
         const token = jwt.sign({ "user" : m_userId, "product" : productId, "isAdmin" : m_userRole }, SecretKey_JWT, { expiresIn: '24h' });
-        return res.json({ code,             "data" : [token] });
+        return res.json({ code, "data" : [token] });
 
     } catch (error) {
         console.error('Database connection failed:', error);
